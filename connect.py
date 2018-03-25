79e54d9a6128e49a2027d7d6781393a88947dd3d
mo-op <manasa.prakashv@gmail.com>
2018-03-25 18:41:45 +0200

    clean up

--- connect.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask.ext.pymongo import PyMongo 
-
-app = Flask(__name__)
-
-app.config['MONGO_DBNAME'] = 'cities'
-app.config['MONGO_URI'] = 'mongodb://dbuser:db00@ds117469.mlab.com:17469/cities'
-
-mongo = PyMongo(app) 	
